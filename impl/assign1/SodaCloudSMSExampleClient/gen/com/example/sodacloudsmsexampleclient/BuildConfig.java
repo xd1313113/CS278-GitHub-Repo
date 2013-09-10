7f9cebec78ff062fdd2c7de77f420083b4537406
Di Xiao <xd1313113@gmail.com>
2013-09-10 11:22:55 -0500

    modify Asgn1 folder name

--- impl/assign1/SodaCloudSMSExampleClient/gen/com/example/sodacloudsmsexampleclient/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.sodacloudsmsexampleclient;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
